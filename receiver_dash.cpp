ede78642764a7b598c5d2011667b8d14551a3c2f
tasniabari <70384737+tasniabari@users.noreply.github.com>
2024-02-04 13:17:18 -0600

    Basic transmission and receiving code
    This code is not optimized for latency/ speed yet, but the receiver connects to the internet and hosts a server. Desired functionality works

---
+++ receiver_dash.cpp
@@ -0,0 +1,252 @@
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "AsyncTCP.h"
+#include <ArduinoJson.h>
+
+esp_now_peer_info_t slave;
+int chan; 
+
+enum MessageType {PAIRING, DATA,};
+MessageType messageType;
+
+int counter = 0;
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+  uint8_t msgType;
+  uint8_t id;
+  int LowThresh;
+  int HighThresh;
+  unsigned int readingId;
+} struct_message;
+
+typedef struct struct_pairing {       // new structure for pairing
+    uint8_t msgType;
+    uint8_t id;
+    uint8_t macAddr[6];
+    uint8_t channel;
+} struct_pairing;
+
+struct_message incomingReadings;
+struct_message outgoingSetpoints;
+struct_pairing pairingData;
+
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+<title>Arc Fault Detection</title>
+</head>
+<body style='background-color: #EEEEEE;'>
+
+<span style='color: #003366;'>
+
+    <h1>Arc Fault Detection</h1>
+    <p>Upstream arc fault detected: <span id='HighThresh'>-</span></p>
+    <p>Downstream arc fault detected: <span id='LowThresh'>-</span></p>
+</span>
+<script>
+    if (!!window.EventSource) {
+        var source = new EventSource('/events');
+        
+        source.addEventListener('open', function(e) {
+        console.log("Events Connected");
+        }, false);
+        source.addEventListener('error', function(e) {
+        if (e.target.readyState != EventSource.OPEN) {
+            console.log("Events Disconnected");
+        }
+        }, false);
+        
+        source.addEventListener('message', function(e) {
+        console.log("message", e.data);
+        }, false);
+        
+        source.addEventListener('new_readings', function(e) {
+        console.log("new_readings", e.data);
+        var obj = JSON.parse(e.data);
+        document.getElementById('HighThresh').innerHTML = obj.HighThresh;
+	    document.getElementById('LowThresh').innerHTML = obj.LowThresh;
+        }, false);
+    }
+
+</script>
+</body>
+</html>)rawliteral";
+
+void readDataToSend() {
+  outgoingSetpoints.msgType = DATA;
+  outgoingSetpoints.id = 0;
+  outgoingSetpoints.LowThresh = random(0, 40);
+  outgoingSetpoints.HighThresh = random(0, 100);
+  outgoingSetpoints.readingId = counter++;
+}
+
+
+// ---------------------------- esp_ now -------------------------
+void printMAC(const uint8_t * mac_addr){
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+}
+
+bool addPeer(const uint8_t *peer_addr) {      // add pairing
+  memset(&slave, 0, sizeof(slave));
+  const esp_now_peer_info_t *peer = &slave;
+  memcpy(slave.peer_addr, peer_addr, 6);
+  
+  slave.channel = chan; // pick a channel
+  slave.encrypt = 0; // no encryption
+  // check if the peer exists
+  bool exists = esp_now_is_peer_exist(slave.peer_addr);
+  if (exists) {
+    // Slave already paired.
+    Serial.println("Already Paired");
+    return true;
+  }
+  else {
+    esp_err_t addStatus = esp_now_add_peer(peer);
+    if (addStatus == ESP_OK) {
+      // Pair success
+      Serial.println("Pair success");
+      return true;
+    }
+    else 
+    {
+      Serial.println("Pair failed");
+      return false;
+    }
+  }
+} 
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("Last Packet Send Status: ");
+  Serial.print(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success to " : "Delivery Fail to ");
+  printMAC(mac_addr);
+  Serial.println();
+}
+
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) { 
+  Serial.print(len);
+  Serial.print(" bytes of data received from : ");
+  printMAC(mac_addr);
+  Serial.println();
+  StaticJsonDocument<1000> root;
+  String payload;
+  uint8_t type = incomingData[0];       // first message byte is the type of message 
+  switch (type) {
+  case DATA :                           // the message is data type
+    memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+    // create a JSON document with received data and send it by event to the web page
+    root["id"] = incomingReadings.id;
+    root["LowThresh"] = incomingReadings.LowThresh;
+    root["HighThresh"] = incomingReadings.HighThresh;
+    root["readingId"] = String(incomingReadings.readingId);
+    serializeJson(root, payload);
+    Serial.print("event send :");
+    serializeJson(root, Serial);
+    events.send(payload.c_str(), "new_readings", millis());
+    Serial.println();
+    break;
+  
+  case PAIRING:                            // the message is a pairing request 
+    memcpy(&pairingData, incomingData, sizeof(pairingData));
+    Serial.println(pairingData.msgType);
+    Serial.println(pairingData.id);
+    Serial.print("Pairing request from: ");
+    printMAC(mac_addr);
+    Serial.println();
+    Serial.println(pairingData.channel);
+    if (pairingData.id > 0) {     // do not replay to server itself
+      if (pairingData.msgType == PAIRING) { 
+        pairingData.id = 0;       // 0 is server
+        // Server is in AP_STA mode: peers need to send data to server soft AP MAC address 
+        WiFi.softAPmacAddress(pairingData.macAddr);   
+        pairingData.channel = chan;
+        Serial.println("send response");
+        esp_err_t result = esp_now_send(mac_addr, (uint8_t *) &pairingData, sizeof(pairingData));
+        addPeer(mac_addr);
+      }  
+    }  
+    break; 
+  }
+}
+
+void initESP_NOW(){
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+      Serial.println("Error initializing ESP-NOW");
+      return;
+    }
+    esp_now_register_send_cb(OnDataSent);
+    esp_now_register_recv_cb(OnDataRecv);
+} 
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.print("Server MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+
+  // Set the device as a Station and Soft Access Point simultaneously
+  WiFi.mode(WIFI_AP_STA);
+  // Set device as a Wi-Fi Station
+  WiFi.begin("Bangla Network", "fearlessjungle420");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Setting as a Wi-Fi Station..");
+  }
+
+  Serial.print("Server SOFT AP MAC Address:  ");
+  Serial.println(WiFi.softAPmacAddress());
+
+  chan = WiFi.channel();
+  Serial.print("Station IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Wi-Fi Channel: ");
+  Serial.println(WiFi.channel());
+
+  initESP_NOW();
+  
+  // Start Web server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+  
+
+  // Events 
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+  
+  // start server
+  server.begin();
+
+}
+
+void loop() {
+  static unsigned long lastEventTime = millis();
+  static const unsigned long EVENT_INTERVAL_MS = 5000;
+  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
+    events.send("ping",NULL,millis());
+    lastEventTime = millis();
+    readDataToSend();
+    esp_now_send(NULL, (uint8_t *) &outgoingSetpoints, sizeof(outgoingSetpoints));
+  }
+}
